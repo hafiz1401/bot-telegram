265d0f6c3d7429f937bebf89e1cd9f710e123716
hiorisz <43258259+hiorisz@users.noreply.github.com>
2018-10-11 09:38:47 +0800

    fix model

--- dbhelper.py
+++ dbhelper.py
@@ -1,106 +1,158 @@
-import pymysql
+import pymysql.cursors
 import os
 import subprocess
 
 class dbhelper:
     def __init__(self, dbname="indi_banua"):
-        self.dbname = dbname
-        self.db = pymysql.connect(host='localhost',user='root',db=dbname)
+        # self.dbname = dbname
+        # self.db = pymysql.connect(host='localhost',user='root',db=dbname)
         # self.db = pymysql.connect(host='192.168.1.146',user='root',db=dbname)
-        self.cursor = self.db.cursor()
+        # self.cursor = self.db.cursor()
+        self.connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db=dbname,
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
 
     def get_all(self):
-        sql = "SELECT * FROM user"
-        
-        self.cursor.execute(sql)
-        return self.cursor.fetchall()
+        # result = ''
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT * FROM user"
+                cursor.execute(sql)
+                result = cursor.fetchall()
+        finally:
+            self.connection.close()
 
+        return result
+   
     def get_user(self,where,tosearch,toget='*'):
-        sql = "SELECT {} FROM user WHERE `{}` = {}".format(toget,where,tosearch)
+        # result = ''
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT {} FROM user WHERE `{}` = {}".format(toget,where,tosearch)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+        finally:
+            self.connection.close()
 
-        self.cursor.execute(sql)
-        return self.cursor.fetchall()
-    
+        return result
+
     def get_user_telegram(self,id_telegram):
-        sql = "SELECT * FROM user_telegram WHERE `id_telegram` = {}".format(id_telegram)
+        # result = ''
+        try:
+            with self.connection.cursor() as cursor:
+                sql = "SELECT * FROM user_telegram WHERE `id_telegram` = {}".format(id_telegram)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+        finally:
+            self.connection.close()
 
-        self.cursor.execute(sql)
-        return self.cursor.fetchall()
+        return result
 
     def get_event_by_otp(self,otp):
-        sql = "SELECT id_event FROM otp WHERE `otp` = {}".format(otp)
-
-        self.cursor.execute(sql)
-        return self.cursor.fetchall()
-
-    def insert_event_participant(self, data):
-        sql = "INSERT INTO `event_participant` (`id_event`, `id_user`) VALUES ('{}','{}');"\
-        .format(*data.values()) 
-        
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return str(e)
+            with self.connection.cursor() as cursor:
+                sql = "SELECT id_event FROM otp WHERE `otp` = {}".format(otp)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+        finally:
+            self.connection.close()
+
+        return result
+        
+    def insert_event_participant(self, data):
+        # result = ''
+        try:
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `event_participant` (`id_event`, `id_user`) VALUES ('{}','{}');".format(*data.values()) 
+                cursor.execute(sql)
+                result = cursor.fetchall()
+
+            connection.commit()
+        finally:
+            self.connection.close()
+
+        return result
 
     def insert(self,nik,name,loker,account_type,created_at,chat_id):
-        sql = "INSERT INTO `user` (`nik`, `name`, `loker`,\
-        `account_type`, `created_at`) VALUES ('{}', '{}', '{}','{}', {});"\
-        .format(nik,name,loker,account_type,created_at) 
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            sqltelegram = "INSERT INTO `user_telegram` ( `id_user`, `id_telegram`) VALUES \
-            ('{}','{}');".format(self.cursor.lastrowid,chat_id)
-            try:
-                self.cursor.execute(sqltelegram)
-                self.db.commit()
-            except Exception as e:
-                self.db.commit()
-                return str(e)
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return str(e)
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `user` (`nik`, `name`, `loker`,\
+                    `account_type`, `created_at`) VALUES ('{}', '{}', '{}','{}', {});"\
+                    .format(nik,name,loker,account_type,created_at) 
+                cursor.execute(sql)
+                sqltelegram = "INSERT INTO `user_telegram` ( `id_user`, `id_telegram`) VALUES \
+                    ('{}','{}');".format(self.cursor.lastrowid,chat_id)
+                cursor.execute(sqltelegram)
+                result = cursor.fetchall()
 
+            connection.commit()
+        finally:
+            self.connection.close()
+
+        return result
+        
+      
     def edit(self,data):
-        sql = "UPDATE `user` SET `name` = '{}', `loker` = '{}' WHERE \
-        `user`.`id_user` = '{}';".format(*data.values())
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return e
+            with connection.cursor() as cursor:
+                sql = "UPDATE `user` SET `name` = '{}', `loker` = '{}' WHERE \
+                    `user`.`id_user` = '{}';".format(*data.values())
+                cursor.execute(sql)
+                result = cursor.fetchall()
+
+            connection.commit()
+        finally:
+            self.connection.close()
+
+        return result
+      
        
     def delete(self,id_user):
-        sql = "DELETE FROM `user` WHERE `id_user` = '{}'".format(id_user)
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return e    
+            with connection.cursor() as cursor:
+                sql = "DELETE FROM `user` WHERE `id_user` = '{}'".format(id_user)
+                cursor.execute(sql)
+                result = cursor.fetchall()
+
+            connection.commit()
+        finally:
+            self.connection.close()
+
+        return result
 
     def bind(self, id_user, id_telegram):
-        sql = "INSERT INTO `user_telegram` (`id_user`,`id_telegram`) VALUES ('{}','{}')".format(id_user,id_telegram)
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return e    
+            with connection.cursor() as cursor:
+                sql = "INSERT INTO `user_telegram` (`id_user`,`id_telegram`) VALUES ('{}','{}')".format(id_user,id_telegram)
+                cursor.execute(sql)
+                result = cursor.fetchall()
 
+            connection.commit()
+        finally:
+            self.connection.close()
+
+        return result
 
     def unbind(self, id_telegram):
-        sql = "DELETE FROM `user_telegram` WHERE `id_telegram` = '{}'".format(id_telegram)
+        # result = ''
         try:
-            self.cursor.execute(sql)
-            self.db.commit()
-            return self.cursor.fetchall()
-        except pymysql.IntegrityError as e:
-            return e    
+            with connection.cursor() as cursor:
+                sql = "DELETE FROM `user_telegram` WHERE `id_telegram` = '{}'".format(id_telegram)
+        
+            connection.commit()
+        finally:
+            self.connection.close()
 
-    
+        return result
+        
     def password_hash(self,pw):
         p = subprocess.Popen(['php',os.getcwd()+'\\genpass.php',pw],shell=True,stdout=subprocess.PIPE)
         result = p.communicate()[0]
